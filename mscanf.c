5d5319daad4f08adc4cfa9b82dc5760795be31f9
Roland-Steven <1347531203@qq.com>
2018-10-19 10:49:58 +0800

    first commit

---
+++ mscanf.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    char c[30], c1;
+    scanf("%s %c",c ,&c1 );
+    printf ("%s%c",c,c1);
+    return 0;
+}
--- test.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    char c[30], c1;
-    scanf("%s %c",c ,&c1 );
-    printf ("%s%c",c,c1);
-    return 0;
-}
