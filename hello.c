d25d9cb67dc51ca2bf1cf71e30ba5eba19c2b3fa
Roland-Cheng <1347531203@qq.com>
2021-01-28 17:35:59 +0800

    2020.1.28

--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
-#include<stdio.h>
-int main ()
+#include <stdio.h>
+#include <math.h>
+int main()
 {
 	int m;
-	scanf("%d",&m);
-	printf("hello world你好世界%d\n",m);
+	scanf("%d", &m);
+	printf("hello world你好世界%d\n", m);
+	printf("%f", pow(2, 3));
 	return 0;
 }
