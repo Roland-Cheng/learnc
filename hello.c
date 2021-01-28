90ba02fe6dfbb40c2b08e76de4778059619b830f
Roland-Steven <1347531203@qq.com>
2021-01-28 17:33:27 +0800

    2020.1.28

--- hello.c
+++ hello.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
-int main ()
+#include <stdio.h>
+int main()
 {
 	int m;
-	scanf("%d",&m);
-	printf("hello world你好世界%d\n",m);
+	scanf("%d", &m);
+	fprintf(fp, "%s", s);
+	printf("hello world你好世界%d\n", m);
 	return 0;
 }
